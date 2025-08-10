d986e86040eab342015740a7786ae2ae87cafda7
littlepac <tkpc071083@gmail.com>
2025-08-10 23:38:27 +0800

    another attempt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -42,7 +42,7 @@ fun Application.module() {
     }
     install(CORS) {
         allowHost("localhost:3000")
-        allowHost("tactx-api-61597259690.us-central1.run.app")
+        allowHost("tactx-api-61597259690.us-central1.run.app", listOf("https"))
         allowCredentials = true  // if you need to send cookies/auth headers
         allowNonSimpleContentTypes = true
 
